738240ab16b87848e48b806772114d89daa39355
sneakie <adam.fredlund@hotmail.com>
2023-02-08 17:26:00 +0100

    Finally figured out everything, buttons work and update the sheet as they should, so all back-end code is basically working, even if it might not be the most clean or elegant setup

--- main.py
+++ main.py
@@ -63,22 +63,26 @@ dp_name_pos = [
 
 ]
 
-# Maybe useless variables
-number = 2
 
-def dp(row, col):
-    position = col+str(row)
+# Dropdown command function (row, col in dp if changing)
+def dp(status, pos):
+    print(status)
+    print(pos)
+    #print(pos)
+    #position = col+str(row)
     #dropdown_option = option
-    sheet_instance.update_acell(position, "Accepted")
+    sheet_instance.update_acell(pos, status)
     return
 
 
+# Column differentiator
 def position(position):
     for letter in letter_List:
         position = letter
         return
 
 
+# Name's of players in A Column
 def label_name():
     row = 0
     for label_name in all_values:
@@ -90,6 +94,7 @@ def label_name():
         # print(label_name[0])
 
 
+# All the dates in row 0 (excluding A-G Column)
 def label_date():
     column = 0
     # var = StringVar()
@@ -105,42 +110,41 @@ def label_date():
         # print(all_values[0][7:14])
         # print(rows[0][7:14])
 
+
+# Function that fills the dp_name list with appropriate names from the google sheet
 def dp_row(p):
         row = sheet_instance.row_values(p)
         for pos in row[7:15]:
             dp_name.append(pos)
-            print(dp_name)
-            #print(len(dp_name))
-            #print(dp_name)
+            # print(dp_name)
+            # print(len(dp_name))
+            # print(dp_name)
 
-# function that creates buttons
+
+# function that creates dropdowns & names them correctly
 def start():
-    #for i in range(0, 48):
-        #print(dp_name[i])
+    # for i in range(0, 48):
+        # print(dp_name[i])
+    o = -1
     for i in range(2, 8):
-        p=i
-        dp_row(p)
+        #p=i
+        #dp_row(p)
         column = 0
         for letter in letter_List:
-            #print(i)
+            pos = f"{letter}{i}"
             column += 1
-            #cell = sheet_instance.cell()
-            #pos = letter + str(i)
-            #coord = sheet_instance.acell(pos).value
+            o +=1
             menu_op = StringVar()
-            menu_op.set(dp_name[i])
-            dropdown = OptionMenu(top, menu_op, *dp_options)
+            menu_op.set(dp_name[o])
+            #dropdown = OptionMenu(top, menu_op, *dp_options)
+            dropdown = OptionMenu(top, menu_op, *dp_options, command=lambda status=dp_options, pos=pos: dp(status, pos))
             dropdown.grid(column=column, row=i, pady=3, padx=2)
-            #dropdown = OptionMenu(top, text=f"{letter}{i}", command=lambda row=i, col=letter: dp(row, col), *dp_options)
-            #dropdown.grid(column=column, row=i, pady=3, padx=2)
-            #mybutton = Button(top, height=2, width=5, text=f"{letter}{i}, command=lambda row=i, col=letter: dp(row, col))
-            #mybutton.grid(column=column, row=i, pady=3, padx=2)
-            # command=lambda row=i, col=letter, option=option: dp(row, col, option), text=dp_options
 
-#pos = letter + str(i)
-#coord = sheet_instance.acell(pos).value
-#list.append(dp_name, coord)
-#print(dp_name)
+
+# Actual code running
+for i in range(2, 8):
+    p = i
+    dp_row(p)
 label_date()
 label_name()
 start()
